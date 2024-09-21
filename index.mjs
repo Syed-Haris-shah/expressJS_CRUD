eab909b03851afee6cba2f6fe2dc8b67afae9628
Syed Haris shah <haris.official222@gmail.com>
2024-09-21 22:59:19 +0500

    make some changes add express json method

--- index.mjs
+++ index.mjs
@@ -3,12 +3,13 @@ import express from 'express'
 const PORT = 3000
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res)=>{
 
     res.json({message: 'hello'})
-
 })
+
 
 app.listen(PORT, ()=>{
     console.log(`server in listning on http://localhost:${PORT}`)
