832534f9624464b595b3eee955032f47ed52ee4f
Syed Haris shah <haris.official222@gmail.com>
2024-09-21 22:55:49 +0500

    server created in express

--- index.mjs
+++ index.mjs
@@ -1,1 +1,15 @@
-console.log('hello')
+import express from 'express'
+
+const PORT = 3000
+
+const app = express()
+
+app.get('/', (req, res)=>{
+
+    res.json({message: 'hello'})
+
+})
+
+app.listen(PORT, ()=>{
+    console.log(`server in listning on http://localhost:${PORT}`)
+})
